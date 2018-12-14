db9b6b896335ae42c56db32cae13490591ae9220
Soura <sdutta@machinesense.com>
2018-12-14 17:53:00 +0530

    separating edge version

--- app1.js
+++ app1.js
@@ -11,48 +11,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function(req, res){
    
-    res.sendFile(__dirname+'/form1.html');
-    //res.sendFile('/usr/src/resource/sensor_onboard/form1.html');
-});
-
-app.post('/dhubOnBoard', function(req, res) {
-
-	var jsondata   = {};
-	var connstr    = req.body.connstr;
-	var dboardlink = req.body.dboardlink;
-	var dhubmac    = req.body.dhubmac.toUpperCase();
-
-	jsondata["datahubmac"]       = dhubmac;
-	jsondata["connectionstring"] = connstr;
-	jsondata["dashboardlink"]    = dboardlink;
-	jsondata["type"]             = "Admin";
-	
-
-	var options = {
-		  uri: 'https://ratmanepcdemo.azurewebsites.net/api/ratmanOnboarding?code=bbKDCcV89u6DfPAats7C1Otfv8FwuKlLgoigYSfr/GmaGLTD3VlTUQ==',
-		  method: 'POST',
-		  json: jsondata
-		};
-		
-	request(options, 
-		(error, resp, body) => {
-		if (error) 
-		    throw error;
-		    
-		if(resp.statusCode == 200 && body != "")
-			if(body.includes("anymore") || body.includes("done"))
-			{
-			    res.sendFile(__dirname+'/form2.html');
-			    //res.sendFile('/usr/src/resource/sensor_onboard/form2.html');
-			    console.log(body);
-			}
-    			
-    		else
-    			res.send("Failed to write to database.");
-    	else
-    		res.send("Failed to send data.");			
-	});
-
+    res.sendFile(__dirname+'/form2.html');
+    //res.sendFile('/usr/src/resource/sensor_onboard/form2.html');
 });
 
 app.post('/onBoard', function(req, res) {
